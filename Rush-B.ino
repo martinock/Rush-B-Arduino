d32357c58530d75ff01f2fddf0f044cc8663b4ae
martinock <martino.aksel.11@gmail.com>
2017-04-02 22:40:11 +0700

    Add PWM

--- Rush-B.ino
+++ Rush-B.ino
@@ -2,40 +2,72 @@
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
 
+const int buttonPin = 9;
+const int ledPin = 11;
 LiquidCrystal_I2C  lcd(0x27,2,1,0,4,5,6,7); // 0x27 is the I2C bus address for an unmodified module
 int sensorPin = A0; // select the input pin for ldr
 int sensorValue = 0; // variable to store the value coming from the sensor
+int buttonValue = HIGH;
+boolean isScreenOff = false;
 unsigned long startTime = 0;
-unsigned long elapsed = 0;
+const long debounceDelay = 500;
+long lastDebounceTime = 0;
+int brightness = 0;
+int fadeAmount = 5;
 
 void setup()
 {
+    //Setup LCD
     lcd.setBacklightPin(3,POSITIVE);
-    lcd.setBacklight(HIGH); // NOTE: You can turn the backlight off by setting it to LOW instead of HIGH
+    lcd.setBacklight(HIGH);
     lcd.begin(16, 2);
     lcd.clear();
-    pinMode(13, OUTPUT);
-    elapsed = millis();
+
+    //Setup Pin
+    pinMode(ledPin, OUTPUT);
+    pinMode(buttonPin, INPUT); 
 }
 
 void loop()
 {
-    if (elapsed - startTime < 1000) {      
+    //Handle Button Input
+    buttonValue = digitalRead(buttonPin);
+
+    //Handle LCD Delay per second
+    if (millis() - startTime < 1000) {   
       sensorValue = analogRead(sensorPin); // read the value from the sensor
       lcd.setCursor(0,0);
-      if (sensorValue < 200) {
-        digitalWrite(13, HIGH);       
+      if (sensorValue < 200) {       
         lcd.print("Low Light");
+        analogWrite(ledPin, brightness);
+        brightness = brightness + fadeAmount;
+        if (brightness <= 0 || brightness >= 255) {
+          fadeAmount = -fadeAmount;
+        }
+        delay(20);
       } else {
-        digitalWrite(13, LOW);
+        digitalWrite(ledPin, LOW);
         lcd.print("Press the button");
         lcd.setCursor(0,1);
         lcd.print("to get the code");
       }
-      elapsed = millis();
     } else {
       lcd.clear();
       startTime = millis();
-      elapsed = millis();
+    }
+
+    //Handle Button Delay
+    if (millis() - lastDebounceTime > debounceDelay) {
+      if (buttonValue == HIGH) {
+        if (!isScreenOff) {
+          lcd.setBacklight(LOW);
+          isScreenOff = true;
+          lastDebounceTime = millis();
+        } else {
+          lcd.setBacklight(HIGH);
+          isScreenOff = false;
+          lastDebounceTime = millis();
+        }
+      }
     }
 }
